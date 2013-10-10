97da7fb7680638d5575208d2bb0ac73ecaf5bd70
Alex Peattie <alexpeattie@gmail.com>
2013-10-10 12:12:45 +0100

    Add nested forms

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -11,11 +11,30 @@ class QuizzesController < ApplicationController
 
   def new
     @quiz = Quiz.new
+    @quiz.questions.build
+  end
+
+  def edit
+    @quiz = Quiz.find params[:id]
+  end
+
+  def update
+    @quiz = Quiz.find params[:id]
+    if @quiz.update params[:quiz].permit(:title)
+      redirect_to @quiz
+    else
+      render 'edit'
+    end
   end
 
   def create
-    quiz = Quiz.create(params[:quiz].permit(:title))
-    redirect_to quiz
+    @quiz = Quiz.new params[:quiz].permit(:title, questions_attributes: [:query])
+
+    if @quiz.save
+      redirect_to @quiz
+    else
+      render 'new'
+    end
   end
 
 end
--- spec/features/quiz_feature_spec.rb
+++ spec/features/quiz_feature_spec.rb
@@ -28,6 +28,7 @@ describe 'the quizzes section' do
   end
 
   describe '/quizzes/new' do
+    
     it 'creates a new quiz' do
       visit '/quizzes/new'
 
@@ -39,6 +40,25 @@ describe 'the quizzes section' do
       expect(current_url).to eq url_for(Quiz.last)
       expect(page).to have_content 'Brand new quiz'
     end
+
+    it 'can also create a new question' do
+      visit '/quizzes/new'
+
+      fill_in 'Title', with: 'Quiz title'
+      fill_in 'Question', with: 'What is your name?'
+      click_button 'Create Quiz'
+
+      expect(page).to have_content 'What is your name?'
+      expect(Quiz.last.questions).not_to be_empty
+    end
+
+    it 'should not accept a new quiz without a title' do
+      visit '/quizzes/new'
+      click_button 'Create Quiz'
+
+      expect(page).to have_content 'error'
+    end
+
   end
 
 end
